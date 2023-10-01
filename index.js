be04dd3e1ecc6a0711ddb35d2dedc24957a2aade
vyasylum <atharvvyas0202@gmail.com>
2023-10-02 05:14:48 +0530

    Adding my animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let contents = []; 
 let cards = [
       //  Add your card in this section
@@ -7,9 +8,16 @@ let cards = [
       imageLink: "./Cards/Keera/keera.gif",
       author: "Harkeerat",
       githubLink: 'https://github.com/ekeiran'
-    }
+    },
+       
+       {
+        artName: "Sakura Magic",
+        pageLink: "./Cards/Atharv/my_animation.html",
+        imageLink: "./Cards/Atharv/atharv.gif",
+        author: "Atharv",
+        githubLink: 'https://github.com/vyasylum'
+      }
   ];
-
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
 // +                      DO NOT NEED TO CHANGE ANYTHING BELOW THIS                 +
@@ -20,17 +28,16 @@ let cards = [
 
 cards.forEach((c) => {
     contents.push([
-      `<li class="card">` +
-        `<a href='${c.pageLink}'>` +
-        `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-        `</a>` +
-        `<div class="flex-content">` +
-        `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-        `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-        `</div>` +
-        `</li>`
+      <li class="card"> +
+        <a href='${c.pageLink}'> +
+        <img class="art-image" src='${c.imageLink}' alt='${c.artName}' /> +
+        </a> +
+        <div class="flex-content"> +
+        <a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a> +
+        <p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p> +
+        </div> +
+        </li>
     ]);
   });
   
-  document.getElementById('cards').innerHTML = contents;
-
+  document.getElementById('cards').innerHTML = contents;
