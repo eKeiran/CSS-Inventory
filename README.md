762d324c533d1ec1f0f5000b1b11a6ca23b4430d
eKeiran <harkeeratdhunda@gmail.com>
2023-10-03 01:58:02 +0530

    read me updated

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # CSS-Inventory
 Collection of CSS Animations!
+
+Made for Hacktoberst at MIT-ADT but anyone is welcome to participate!!
+This is basically a display of CSS animations inspired by ZTM's Animation Challenge!
+
+## RULES:
+1. Make a folder after yourself in the Cards directory
+2. Write your `my_animation.html` and `my_animation.css` files containing your animation and link them using `<style>`; no `<script>` allowed! Just pure CSS. You can be as imaginative as you want with your animation.
+3. Once done with the `.html` and `.css` files, screen record your animation and export it as a gif and save it inside your folder as `"your_name.gif"`. I'd recommend using OBS Studio to record then an online gif converter OR Microsoft ClipChamp - which can export a screen recorded video as a gif easily!  
+4. Great! You've made it halfway if you've done all this. Now just go to `index.js` and in the cards array, add your element separated by a comma:
+
+```javascript
+{
+  artName: "name_your_animation",
+  pageLink: "./Cards/your_name/my_animation.html",
+  imageLink: "./Cards/your_name/your_gif_name.gif",
+  author: "your_name",
+  githubLink: 'https://github.com/your_username'
+}
+
+All done!! Your changes should be reflected soon! PLEASE BE CAREFUL WHILE LINKING YOUR CREATION, USE YOUR OWN FOLDER.
+NOTE: PLEASE REMEMBER YOU NEED TO ADD A GIF OF YOUR ANIMATION TO MAKE IT SHOW UP ON THE MAIN WEBSITE. THANKS!
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-
-let contents = []; 
+let contents = '';
 let cards = [
-      //  Add your card in this section
-    {
-      artName: "Hack-o-ween",
-      pageLink: "./Cards/Keera/my_animation.html",
-      imageLink: "./Cards/Keera/keera.gif",
-      author: "Harkeerat",
-      githubLink: 'https://github.com/ekeiran'
-    },
-       
-       {
-        artName: "Sakura Magic",
-        pageLink: "./Cards/Atharv/my_animation.html",
-        imageLink: "./Cards/Atharv/atharv.gif",
-        author: "Atharv",
-        githubLink: 'https://github.com/vyasylum'
-      }
-  ];
-// +--------------------------------------------------------------------------------+
-// +                                                                                +
-// +                      DO NOT NEED TO CHANGE ANYTHING BELOW THIS                 +
-// +                                                                                +
-// +--------------------------------------------------------------------------------+
+  // Add your card objects here
+  {
+    artName: "Hack-o-ween",
+    pageLink: "./Cards/Keera/my_animation.html",
+    imageLink: "./Cards/Keera/keera.gif",
+    author: "Harkeerat",
+    githubLink: 'https://github.com/ekeiran'
+  },
+  {
+    artName: "Sakura Magic",
+    pageLink: "./Cards/Atharv/my_animation.html",
+    imageLink: "./Cards/Atharv/atharv.gif",
+    author: "Atharv",
+    githubLink: 'https://github.com/vyasylum'
+  },
+];
 
-// Creates cards from the array above
-
+// Create cards from the array above
 cards.forEach((c) => {
-    contents.push([
-      <li class="card"> +
-        <a href='${c.pageLink}'> +
-        <img class="art-image" src='${c.imageLink}' alt='${c.artName}' /> +
-        </a> +
-        <div class="flex-content"> +
-        <a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a> +
-        <p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p> +
-        </div> +
-        </li>
-    ]);
-  });
-  
-  document.getElementById('cards').innerHTML = contents;
+  contents += `
+    <li class="card">
+      <a href="${c.pageLink}">
+        <img class="art-image" src="${c.imageLink}" alt="${c.artName}" />
+      </a>
+      <div class="flex-content">
+        <a href="${c.pageLink}"><h3 class="art-title">${c.artName}</h3></a>
+        <p class="author"><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a></p>
+      </div>
+    </li>`;
+});
+
+document.getElementById('cards').innerHTML = contents;
